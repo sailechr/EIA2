764f2e8aaa00c5444f8585c61a28d310b3f12667
Chris <Chris@LAPTOP-O18RKFBR.hfu-open.fu.hs-furtwangen.de>
2017-03-17 13:53:33 +0100

    aufgabe0

---
+++ aufgabe0/web.js
@@ -0,0 +1,9 @@
+//Beginn
+document.addEventListener('DOMContentLoaded', function () {
+    var name;
+    name = prompt("Bitte geben Sie ihren Namen ein :)");
+    if (name) {
+        alert("Hallo" + name + ":)");
+    }
+});
+//# sourceMappingURL=web.js.map
---
+++ aufgabe0/web.ts
@@ -0,0 +1,13 @@
+//Beginn
+document.addEventListener('DOMContentLoaded', function() {
+   
+
+var name: string;
+
+name = prompt("Bitte geben Sie ihren Namen ein :)");
+
+if (name)
+{ alert("Hallo" +  name  + ":)"); }
+
+
+});
