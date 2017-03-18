4bf24852a99051953603875d0b2efdad6e2c1c52
Chris <christian.kosselek@web.de>
2017-03-18 18:36:43 +0100

    Add files via upload

--- web.ts
+++ web.ts
@@ -1,4 +1,13 @@
 //Beginn
+//Aufgabe: 0
+//Name: Saile Christian
+//Matrikel: 254877
+//Datum: 18.März 2017 
+//Hiermit versichere ich, dass ich diesen
+//Code selbst geschrieben habe. Er wurde
+//nicht kopiert und auch nicht diktiert.
+
+
 document.addEventListener('DOMContentLoaded', function () {
     var name: string;
     name = prompt("Bitte geben Sie ihren Namen ein :)");
