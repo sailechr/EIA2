3d653291171d67894d52f077b7d60c7cfb6cfc95
Chris <Chris@LAPTOP-O18RKFBR.Speedport_W_724V_01011603_05_020>
2017-04-02 16:30:23 +0200

    Aufgabe 2

---
+++ Aufgabe2/schachbrett.js
@@ -0,0 +1,36 @@
+//Aufgabe 2
+//Name: saile christian
+//Matrikel: 254877
+//Datum: 2.April 2017
+//    
+//Hiermit versichere ich, 
+//dass ich diesen Code selbst geschrieben habe. 
+//Er wurde nicht kopiert und auch nicht diktiert.
+document.addEventListener('DOMContentLoaded', function () {
+    let zahlRice = 1;
+    for (let i = 0; i < 64; i++) {
+        zahlRice.toExponential(5);
+        zahlRice = zahlRice * 2;
+        zahlRice.toString();
+    }
+    //   if(i%2 == 0 ){
+    //        = zahlRice;
+    //        }
+    //    if(i%2 == 1)
+    //        {div.style.backgroundColor = "white";
+    //    }
+    //    if(zeile %2 == 0){
+    //   if(i%2 == 0){
+    //       div.style.backgroundColor = "white"
+    //        }
+    //   if(i%2 == 1)
+    //        {div.style.backgroundColor = "black";
+    //}
+    //}
+    //    
+    // 
+    // }
+    //for(let zeile: number =0; zeile <8; zeile++)
+    //{if(zeile % 2==0);}
+});
+//# sourceMappingURL=schachbrett.js.map
---
+++ Aufgabe2/schachbrett.ts
@@ -0,0 +1,53 @@
+//Aufgabe 2
+//Name: saile christian
+//Matrikel: 254877
+//Datum: 2.April 2017
+//    
+//Hiermit versichere ich, 
+//dass ich diesen Code selbst geschrieben habe. 
+//Er wurde nicht kopiert und auch nicht diktiert.
+document.addEventListener('DOMContentLoaded', function () {
+let zahlRice: number=1;
+
+
+for (let i: number = 0; i < 64; i++) {
+    zahlRice.toExponential(5);
+    zahlRice= zahlRice *2;
+    zahlRice.toString();
+   document.write(
+     "<tr><td>" + zahlRice + "</td></tr>"  
+);}
+//    if(className="black")
+//    {   
+//    
+//    else{    document.write(
+//      "<table><tr><td>" + zahlRice + "</td></tr></table>"
+//    );}
+    } 
+   
+  
+
+//   if(i%2 == 0 ){
+//        = zahlRice;
+//        }
+//    if(i%2 == 1)
+//        {div.style.backgroundColor = "white";
+//    }
+//    if(zeile %2 == 0){
+//   if(i%2 == 0){
+//       div.style.backgroundColor = "white"
+//        }
+//   if(i%2 == 1)
+//        {div.style.backgroundColor = "black";
+//}
+//}
+//    
+// 
+// }
+//for(let zeile: number =0; zeile <8; zeile++)
+//{if(zeile % 2==0);}
+
+
+});
+
+
