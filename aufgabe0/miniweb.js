333d5f482f70c2398f43f5fcf6955679a9405467
Chris <Chris@LAPTOP-O18RKFBR.Speedport_W_724V_01011603_05_032>
2017-10-10 19:24:03 +0200

    aufgabe 0 wintersemester 2017

---
+++ aufgabe0/miniweb.js
@@ -0,0 +1,16 @@
+//Aufgabe: 0
+//Name: Saile Christian
+//Matrikel: 254877
+//Datum: 10.Oktober 2017 
+//Hiermit versichere ich, dass ich diesen
+//Code selbst geschrieben habe. Er wurde
+//nicht kopiert und auch nicht diktiert.
+document.addEventListener("DOMContentLoaded", function () {
+    let name;
+    name = prompt("Bitte geben Sie ihren Namen ein :)");
+    if (name) {
+        alert("Hallo" + name + ":)");
+    }
+    document.getElementById("eingabename").innerHTML = name;
+});
+//# sourceMappingURL=miniweb.js.map
