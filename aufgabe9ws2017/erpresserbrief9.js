d8ebf86e3a4a94134c75b40589044c237cdc61f0
Chris <Chris@LAPTOP-O18RKFBR.Speedport_W_724V_01011603_05_033>
2017-12-17 19:59:36 +0100

    neu

--- aufgabe9ws2017/erpresserbrief9.js
+++ aufgabe9ws2017/erpresserbrief9.js
@@ -10,12 +10,15 @@ var Aufgabe9;
     window.onload = init; // Wenn die Seite komplett geladen ist, führe die init Funktion aus
     let section;
     let div;
-    let b;
     function init() {
         console.log("Hallo ich bin Sushi");
+        //tastatur wird gezeichnet
         drawQuadrat((Math.random() * 10), (Math.random() * 4), "orange", 50, 50);
+        //Sektion wird kreiert
         section = document.createElement("section");
         document.body.appendChild(section);
+        let b;
+        //styleeigenschaften
         b = section.style;
         b.borderStyle = "solid";
         b.borderColor = "red";
@@ -27,14 +30,21 @@ var Aufgabe9;
         b.display = "inline-block";
         b.width = 1350 + "px";
         b.height = 1200 + "px";
+        //sektion bekommt einen Listener, der auf eine Mausbewegung reagiert
         section.addEventListener("mousedown", MousedownEvent);
+        //Aufruf funktion für tastatur, die auf Tastendruck-events reagiert
+        //funktion wird an das dokument gehängt
+        //onkeydown =The onkeydown event occurs when the user is pressing a key (on the keyboard).
+        //wird ausgeführt, wenn man eine Taste auf der Tastatur drückt
         document.onkeydown = KeyboardEvent;
         for (let i = 65; i <= 90; i++) {
             let alphabet = String.fromCharCode(i);
+            //aus numerischen Parametern wird eine entsprechende Zeichenkette erzeugt a=65 und z =90
             console.log(alphabet);
         }
     }
     function drawQuadrat(_x, _y, _color, _sizeW, _sizeH) {
+        //tastatur aus div-boxen wird erstellet
         for (let i = 0; i < 48; i++) {
             div = document.createElement("div");
             document.body.appendChild(div);
@@ -56,71 +66,80 @@ var Aufgabe9;
             div.textContent = alphabet;
         }
     }
+    //funktion für den Mausklick, die div-boxen mit den passenden buchstaben in der Sektion erstellt
     function MousedownEvent(_event) {
-        if (_event.srcElement.textContent) {
-            console.log(_event);
-            console.log(_event.srcElement);
-            let div = document.createElement("div");
-            document.body.appendChild(div);
-            let s = div.style;
-            s.borderStyle = "solid";
-            s.borderColor = "white";
-            s.borderWidth = "1px";
-            s.backgroundColor = "whitesmoke";
-            s.color = "orange";
-            s.marginTop = "57px";
-            s.position = "absolute";
-            s.width = 50 + "px";
-            s.height = 50 + "px";
-            s.left = _event.screenX.toString();
-            s.top = _event.screenY.toString();
-            div.addEventListener("mousedown", removeInput);
-            var outString = div.textContent;
-            div.textContent = _event.srcElement.textContent;
-            outString += _event.srcElement.textContent;
-            section.textContent = outString;
-        }
+        console.log(_event);
+        console.log(_event.srcElement);
+        //Deklaration einer variablen, die eine DIV-box erstellt
+        let div = document.createElement("div");
+        //erstellte es Element wird dynamisch an den DOM gehängt
+        document.body.appendChild(div);
+        let s = div.style;
+        s.borderStyle = "solid";
+        s.borderColor = "white";
+        s.borderWidth = "1px";
+        s.backgroundColor = "whitesmoke";
+        s.color = "orange";
+        s.marginTop = "57px";
+        s.position = "absolute";
+        s.width = 50 + "px";
+        s.height = 50 + "px";
+        s.left = _event.screenX.toString();
+        s.top = _event.screenY.toString();
+        div.addEventListener("mousedown", removeInput);
+        var outString = div.textContent;
+        div.textContent = _event.srcElement.textContent;
+        outString += _event.srcElement.textContent;
+        section.textContent = outString;
     }
+    //verändert aussehen der tastatur
     function changeColor(_event) {
+        //div-tastatur =zugriff
         div = _event.srcElement;
         if (div) {
             this.classList.toggle("div");
         }
     }
+    //funktion für die Tastatureingabe
     function KeyboardEvent(_event) {
-        if (_event.keyCode) {
-            let div = document.createElement("div");
-            let s = div.style;
-            s.borderStyle = "solid";
-            s.borderColor = "white";
-            s.borderWidth = "1px";
-            s.backgroundColor = "whitesmoke";
-            s.color = "orange";
-            s.marginLeft = "3px";
-            s.position = "absolute";
-            s.marginTop = "5px";
-            s.display = "inline-block";
-            s.position = "absolute";
-            s.width = 50 + "px";
-            s.height = 50 + "px";
-            div.addEventListener("mousedown", removeInput);
-            console.log(_event.keyCode);
-            console.log(_event);
-            var outString = div.textContent;
-            outString += String.fromCharCode(_event.keyCode);
-            section.textContent = outString;
-            document.body.appendChild(div);
-        }
+        let div = document.createElement("div");
+        let s = div.style;
+        s.borderStyle = "solid";
+        s.borderColor = "white";
+        s.borderWidth = "1px";
+        s.backgroundColor = "whitesmoke";
+        s.color = "orange";
+        s.marginLeft = "3px";
+        s.position = "absolute";
+        s.marginTop = "5px";
+        s.display = "inline-block";
+        s.position = "absolute";
+        s.width = 50 + "px";
+        s.height = 50 + "px";
+        div.addEventListener("mousedown", removeInput);
+        console.log(_event.keyCode);
+        console.log(_event);
+        //deklaration der variablen outstring vom typ string, die die unicodzahlen in die buchstaben umwandelt
+        var outString = div.textContent;
+        //Wertzuweisung, und zwar der Inhalt der divboxen
+        outString += String.fromCharCode(_event.keyCode);
+        //der sektion werden div-boxen mit buchstaben hinzugefügt bei jedem klick auf die sektion
+        section.textContent = outString;
+        document.body.appendChild(div);
     }
+    //funktion, die die werte löschen sollte
     function removeInput(_event) {
         console.log(_event.altKey);
+        //funktioniert nicht und ich habe keine ahnung warum :(
+        //weiß nicht mehr weiter
         if (_event.keyCode == 18) {
-            _event.cancelBubble = true;
-            _event.returnValue = false;
-            let div = _event.target;
-            document.removeChild(div);
+            deleteInput(_event);
         }
-        return _event.returnValue;
+    }
+    function deleteInput(_event) {
+        let _delete = _event.target;
+        document.addEventListener("keypressed", removeInput);
+        _delete = document.body.removeChild(div);
     }
 })(Aufgabe9 || (Aufgabe9 = {}));
 //# sourceMappingURL=erpresserbrief9.js.map
--- aufgabe9ws2017/erpresserbrief9.ts
+++ aufgabe9ws2017/erpresserbrief9.ts
@@ -16,7 +16,7 @@ namespace Aufgabe9 {
     window.onload = init; // Wenn die Seite komplett geladen ist, führe die init Funktion aus
     let section: HTMLElement;
     let div: HTMLDivElement;
-    let b: CSSStyleDeclaration;
+
 
 
     function init(): void {
@@ -24,14 +24,16 @@ namespace Aufgabe9 {
 
         console.log("Hallo ich bin Sushi");
 
-
+        //tastatur wird gezeichnet
         drawQuadrat((Math.random() * 10), (Math.random() * 4), "orange", 50, 50);
+
+        //Sektion wird kreiert
         section = document.createElement("section");
 
 
         document.body.appendChild(section);
-
-
+        let b: CSSStyleDeclaration;
+        //styleeigenschaften
         b = section.style;
         b.borderStyle = "solid";
         b.borderColor = "red";
@@ -44,9 +46,14 @@ namespace Aufgabe9 {
         b.display = "inline-block";
         b.width = 1350 + "px";
         b.height = 1200 + "px";
-
+        //sektion bekommt einen Listener, der auf eine Mausbewegung reagiert
         section.addEventListener("mousedown", MousedownEvent);
+        //Aufruf funktion für tastatur, die auf Tastendruck-events reagiert
+        //funktion wird an das dokument gehängt
+        //onkeydown =The onkeydown event occurs when the user is pressing a key (on the keyboard).
+        //wird ausgeführt, wenn man eine Taste auf der Tastatur drückt
         document.onkeydown = KeyboardEvent;
+
 
 
 
@@ -54,17 +61,21 @@ namespace Aufgabe9 {
 
         for (let i: number = 65; i <= 90; i++) {
             let alphabet: string = String.fromCharCode(i);
+
+            //aus numerischen Parametern wird eine entsprechende Zeichenkette erzeugt a=65 und z =90
             console.log(alphabet);
         }
 
     }
     function drawQuadrat(_x: number, _y: number, _color: string, _sizeW: number, _sizeH: number): void {
+        //tastatur aus div-boxen wird erstellet
 
         for (let i: number = 0; i < 48; i++) {
 
             div = document.createElement("div");
 
             document.body.appendChild(div);
+
             div.addEventListener("mousedown", MousedownEvent);
             div.addEventListener("mousedown", changeColor);
 
@@ -108,105 +119,121 @@ namespace Aufgabe9 {
 
 
 
+
     }
+    //funktion für den Mausklick, die div-boxen mit den passenden buchstaben in der Sektion erstellt
     function MousedownEvent(_event: MouseEvent): void {
-        if (_event.srcElement.textContent) {
-            console.log(_event);
-            console.log(_event.srcElement);
-            let div: HTMLDivElement = document.createElement("div");
+
+        console.log(_event);
+        console.log(_event.srcElement);
+        //Deklaration einer variablen, die eine DIV-box erstellt
+        let div: HTMLDivElement = document.createElement("div");
+
+        //erstellte es Element wird dynamisch an den DOM gehängt
+        document.body.appendChild(div);
+
+        let s: CSSStyleDeclaration = div.style;
+        s.borderStyle = "solid";
+        s.borderColor = "white";
+        s.borderWidth = "1px";
+        s.backgroundColor = "whitesmoke";
+        s.color = "orange";
+
+        s.marginTop = "57px";
+
+        s.position = "absolute";
+        s.width = 50 + "px";
+        s.height = 50 + "px";
+        s.left = _event.screenX.toString();
+        s.top = _event.screenY.toString();
+        div.addEventListener("mousedown", removeInput);
 
 
-            document.body.appendChild(div);
+        var outString: string = div.textContent;
 
-            let s: CSSStyleDeclaration = div.style;
-            s.borderStyle = "solid";
-            s.borderColor = "white";
-            s.borderWidth = "1px";
-            s.backgroundColor = "whitesmoke";
-            s.color = "orange";
+        div.textContent = _event.srcElement.textContent;
+        outString += _event.srcElement.textContent;
 
-            s.marginTop = "57px";
-
-            s.position = "absolute";
-            s.width = 50 + "px";
-            s.height = 50 + "px";
-            s.left = _event.screenX.toString();
-            s.top = _event.screenY.toString();
-            div.addEventListener("mousedown", removeInput);
-
-
-            var outString: string = div.textContent;
-
-            div.textContent = _event.srcElement.textContent;
-            outString += _event.srcElement.textContent;
-
-            section.textContent = outString;
-
-
-
-
+        section.textContent = outString;
 
 
 
-        }
+
+
+
+
+
     }
+    //verändert aussehen der tastatur
     function changeColor(_event: MouseEvent): void {
-
+        //div-tastatur =zugriff
         div = <HTMLDivElement>_event.srcElement;
+
         if (div) {
             this.classList.toggle("div");
+            //toggle=umschalten
+            //wenn auf die div-tastatur geklickt wird, dann ändert sich die Schriftfarbe
         }
 
 
 
     }
-
+    //funktion für die Tastatureingabe
     function KeyboardEvent(_event: KeyboardEvent): void {
-        if (_event.keyCode) {
-            let div: HTMLDivElement = document.createElement("div");
+
+        let div: HTMLDivElement = document.createElement("div");
 
 
-            let s: CSSStyleDeclaration = div.style;
-            s.borderStyle = "solid";
-            s.borderColor = "white";
-            s.borderWidth = "1px";
-            s.backgroundColor = "whitesmoke";
-            s.color = "orange";
-            s.marginLeft = "3px";
-            s.position = "absolute";
-            s.marginTop = "5px";
-            s.display = "inline-block";
-            s.position = "absolute";
-            s.width = 50 + "px";
-            s.height = 50 + "px";
-            div.addEventListener("mousedown", removeInput);
+        let s: CSSStyleDeclaration = div.style;
+        s.borderStyle = "solid";
+        s.borderColor = "white";
+        s.borderWidth = "1px";
+        s.backgroundColor = "whitesmoke";
+        s.color = "orange";
+        s.marginLeft = "3px";
+        s.position = "absolute";
+        s.marginTop = "5px";
+        s.display = "inline-block";
+        s.position = "absolute";
+        s.width = 50 + "px";
+        s.height = 50 + "px";
+        div.addEventListener("mousedown", removeInput);
 
 
-            console.log(_event.keyCode);
-            console.log(_event);
-            var outString: string = div.textContent;
+        console.log(_event.keyCode);
+        console.log(_event);
+        //deklaration der variablen outstring vom typ string, die die unicodzahlen in die buchstaben umwandelt
+        var outString: string = div.textContent;
+        //Wertzuweisung, und zwar der Inhalt der divboxen
 
 
-            outString += String.fromCharCode(_event.keyCode);
+        outString += String.fromCharCode(_event.keyCode);
+        //der sektion werden div-boxen mit buchstaben hinzugefügt bei jedem klick auf die sektion
+        section.textContent = outString;
+        document.body.appendChild(div);
 
-            section.textContent = outString;
-            document.body.appendChild(div);
 
-        }
     }
-    function removeInput(_event: KeyboardEvent): boolean {
+    //funktion, die die werte löschen sollte
+    function removeInput(_event: KeyboardEvent): void {
 
         console.log(_event.altKey);
-
+        //funktioniert nicht und ich habe keine ahnung warum :(
+        //weiß nicht mehr weiter
 
         if (_event.keyCode == 18) {
-            _event.cancelBubble = true;
-            _event.returnValue = false;
-            let div: HTMLDivElement = <HTMLDivElement>_event.target;
-            document.removeChild(div);
+            deleteInput(_event);
 
         }
-        return _event.returnValue;
+
+
+    }
+
+
+    function deleteInput(_event: KeyboardEvent): void {
+        let _delete: EventTarget = <HTMLDivElement>_event.target;
+        document.addEventListener("keypressed", removeInput);
+        _delete = document.body.removeChild(div);
 
     }
 
